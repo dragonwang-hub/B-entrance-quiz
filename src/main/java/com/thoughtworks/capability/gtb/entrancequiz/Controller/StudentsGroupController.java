4a91149364f2dac893f01355c1388bc58092f155
dragonwang-hub <915614879@qq.com>
2020-10-27 23:39:34 +0800

    [dragon] feat:增加crossorigin

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Controller/StudentsGroupController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/Controller/StudentsGroupController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StudentsGroupController {
 
